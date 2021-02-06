8939ddc378e8941287cbe398a1a40ddc5d38e7e6
JuanF2019 <juan.martinez27@correo.icesi.edu.co>
2021-02-06 12:30:12 -0500

    Add table functionality

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,6 +34,8 @@ namespace statistical_reports
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -57,17 +59,31 @@ namespace statistical_reports
             resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            // 
             // Form1
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,6 +95,7 @@ namespace statistical_reports
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace statistical_reports
 {
@@ -22,6 +23,29 @@ namespace statistical_reports
             openFileDialog1.ShowDialog();
 
             label2.Text = openFileDialog1.FileName;
+
+            String content = File.ReadAllText(openFileDialog1.FileName);
+
+            DataTable dt = new DataTable();
+
+            String[] splitContent = content.Split('\n');
+
+            String[] splitLine = splitContent[0].Split(',');
+
+            dt.Columns.Add(splitLine[0]);
+            dt.Columns.Add(splitLine[1]);
+            dt.Columns.Add(splitLine[2]);
+            dt.Columns.Add(splitLine[3]);
+            dt.Columns.Add(splitLine[4]);
+
+            for(int i = 1; i < splitContent.Length; i++)
+            {
+                splitLine = splitContent[i].Split(',');
+
+                dt.Rows.Add(splitLine);
+            }
+
+            dataGridView1.DataSource = dt;
         }
     }
 }
