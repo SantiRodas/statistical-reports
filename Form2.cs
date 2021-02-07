1be2b26a0366e2eb4ca556efbe6032b3eb704319
JuanF2019 <juan.martinez27@correo.icesi.edu.co>
2021-02-06 22:33:50 -0500

    Finish pie chart

--- Form2.cs
+++ Form2.cs
@@ -18,8 +18,6 @@ namespace statistical_reports
         {
             InitializeComponent();
 
-            SortedList sl = new SortedList();
-
             Hashtable dic = new Hashtable();
 
             DataRowCollection rows = dt.Rows;
@@ -51,7 +49,7 @@ namespace statistical_reports
 
             bool check = en.MoveNext();
 
-            ArrayList list = new ArrayList();
+            List<KeyValuePair<int,string>> list = new List<KeyValuePair<int,string>>();
 
             chart1.Titles.Add("Report 2");
 
@@ -67,13 +65,36 @@ namespace statistical_reports
 
             list.Sort(Compare1);
 
+            int count2 = 0;
+
+            for (int i = 12; i < list.Count; i++)
+            {
+                count2 += list[i].Key;
+            }
+
+            chart1.Series["s1"].Points.AddXY("OTHERS", count2);
+
+            for(int i = 0; i < list.Count && i < 12; i++)
+            {
+                chart1.Series["s1"].Points.AddXY(list[i].Value, list[i].Key);
+            }
+
+            chart1.Series["s1"].Label = "#VALY";
+            chart1.Series["s1"].LegendText = "#VALX";
         }
 
         static int Compare1(KeyValuePair<int, string> a, KeyValuePair<int, string> b)
         {
+            int compare = a.Key.CompareTo(b.Key);
 
-            return a.Key.CompareTo(b.Key);
-
+            if (compare == 0)
+            {
+                return -1*a.Value.CompareTo(b.Value);
+            }
+            else
+            {
+                return -1*compare;
+            }
         }
 
     }
